778d6bcd4a68c1535a511e741c9e4a97a1a9df1f
ThikerXCX <rifkimuhammad967@gmail.com>
2022-03-21 19:35:25 +0700

    test aja

--- app/src/main/java/com/muhamadrifki/emailform/MainActivity.java
+++ app/src/main/java/com/muhamadrifki/emailform/MainActivity.java
@@ -3,6 +3,9 @@ package com.muhamadrifki.emailform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnKirim = (Button) findViewById(R.id.btnKirim);
+
+        btnKirim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText textKepada = (EditText) findViewById(R.id.textKepada);
+                String kepada = textKepada.getText().toString();
+                EditText textSubjek = (EditText) findViewById(R.id.textSubjek);
+                String subjek = textSubjek.getText().toString();
+                EditText textPesan = (EditText) findViewById(R.id.textPesan);
+                String pesan = textPesan.getText().toString();
+            }
+        });
     }
 }
