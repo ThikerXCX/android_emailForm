7b532c0121ea37003e65b3d50dc15a15fe6cca1d
ThikerXCX <rifkimuhammad967@gmail.com>
2022-03-22 16:36:47 +0700

    test aja

--- app/src/main/java/com/muhamadrifki/emailform/MainActivity.java
+++ app/src/main/java/com/muhamadrifki/emailform/MainActivity.java
@@ -2,6 +2,8 @@ package com.muhamadrifki.emailform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
                 String subjek = textSubjek.getText().toString();
                 EditText textPesan = (EditText) findViewById(R.id.textPesan);
                 String pesan = textPesan.getText().toString();
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("mailto:"+kepada));
+                intent.putExtra(Intent.EXTRA_SUBJECT,subjek);
+                intent.putExtra(Intent.EXTRA_TEXT,pesan);
+                startActivity(intent);
             }
         });
     }
